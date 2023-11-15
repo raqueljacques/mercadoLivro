3a27d2bfbcbb39f0cbcc53d9bc1b374acdb07f03
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2023-11-15 17:48:41 -0300

    feat: adiciona paginacao em books

--- src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
+++ src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
@@ -3,11 +3,14 @@ package com.mercadolivro.repository
 import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-interface BookRepository : CrudRepository<BookModel, Int> {
-
-    fun findByStatus(status: BookStatus): List<BookModel>
+interface BookRepository : JpaRepository<BookModel, Int> {
+//TODO usando JpaRepository existem metodos prontos para uso, como o findAll, findById, save, delete, etc. E resolve a paginação
+    fun findByStatus(status: BookStatus, pageable: Pageable): Page<BookModel>
     fun findByCustomer(customer: CustomerModel) : List<BookModel>
 
 }
