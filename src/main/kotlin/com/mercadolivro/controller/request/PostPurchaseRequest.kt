49f8dd4a01106ebeffef8c6de08fbef0a3b2aefe
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2023-11-16 00:08:26 -0300

    feat: modela e cria endpoint de compra

---
+++ src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
@@ -0,0 +1,15 @@
+package com.mercadolivro.controller.request
+
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
+
+data class PostPurchaseRequest (
+
+    @field:NotNull
+    @field:Positive
+    val customerId: Int,
+
+    @field:NotNull
+    val bookIds: Set<Int>
+
+)
---
+++ src/main/kotlin/com/mercadolivro/model/PurchaseModel.kt
@@ -0,0 +1,32 @@
+package com.mercadolivro.model
+
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+data class PurchaseModel(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    val customerModel: CustomerModel,
+
+    @ManyToMany
+    @JoinTable(
+        name = "purchase_book",
+        joinColumns = [JoinColumn(name = "purchase_id")],
+        inverseJoinColumns = [JoinColumn(name = "book_id")]
+    )
+    val books: List<BookModel>,
+
+    @Column
+    val nfe: String,
+
+    @Column
+    val price: BigDecimal,
+
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime
+)
